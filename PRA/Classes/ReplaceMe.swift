80c4ec3a3c92841c4ab836cdf6c6fb4e944c16dc
Pranesh Vallabh <pvallabh@oldmutual.com>
2023-01-06 18:59:56 +0200

    dd

---
+++ PRA/Classes/ReplaceMe.swift
@@ -0,0 +1,9 @@
+public struct HomeAddress {
+    public let street: String
+    public let town: String
+
+    public init(street: String, town: String) {
+        self.street = street
+        self.town = town
+    }
+}
--- Package.swift
+++ Package.swift
@@ -20,9 +20,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "PRA",
-            dependencies: []),
-        .testTarget(
-            name: "PRATests",
-            dependencies: ["PRA"]),
+            dependencies: [],
+            path: "PRA")
     ]
 )
