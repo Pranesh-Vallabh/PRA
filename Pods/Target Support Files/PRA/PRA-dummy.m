865b1e02068fc3bceb551ce503a0d3296a6551d2
Pranesh Vallabh <pvallabh@oldmutual.com>
2023-01-06 17:29:43 +0200

    initial commit

---
+++ Pods/Target Support Files/PRA/PRA-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_PRA : NSObject
+@end
+@implementation PodsDummy_PRA
+@end
---
+++ Sources/PRA/PRA.swift
@@ -0,0 +1,6 @@
+public struct PRA {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
